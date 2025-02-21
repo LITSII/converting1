c891b3f68278c3752e63770b51417c71b985f5ac
LITSII <jj417hom2000@naver.com>
2025-02-21 11:11:57 +0900

    build secound

--- src/main/java/com/makers/princemaker/dto/PrinceDetailDto.kt
+++ src/main/java/com/makers/princemaker/dto/PrinceDetailDto.kt
@@ -1,41 +1,49 @@
-package com.makers.princemaker.dto;
+package com.makers.princemaker.dto
 
-import com.makers.princemaker.code.StatusCode;
-import com.makers.princemaker.entity.Prince;
-import com.makers.princemaker.type.PrinceLevel;
-import com.makers.princemaker.type.SkillType;
-import lombok.*;
-
-import static com.makers.princemaker.util.DateTimeUtilsKt.getLocalDateTimeString;
+import com.makers.princemaker.code.StatusCode
+import com.makers.princemaker.entity.Prince
+import com.makers.princemaker.entity.Prince.age
+import com.makers.princemaker.entity.Prince.experienceYears
+import com.makers.princemaker.entity.Prince.name
+import com.makers.princemaker.entity.Prince.princeId
+import com.makers.princemaker.entity.Prince.princeLevel
+import com.makers.princemaker.entity.Prince.skillType
+import com.makers.princemaker.entity.Prince.status
+import com.makers.princemaker.type.PrinceLevel
+import com.makers.princemaker.type.SkillType
+import com.makers.princemaker.util.getLocalDateTimeString
 
 /**
  * @author Snow
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class PrinceDetailDto {
-    private PrinceLevel princeLevel;
-    private SkillType skillType;
-    private Integer experienceYears;
-    private String princeId;
-    private String name;
-    private Integer age;
-    private StatusCode status;
-    private String birthDate;
+class PrinceDetailDto {
+    var princeLevel: PrinceLevel? = null
+    var skillType: SkillType? = null
+    var experienceYears: Int? = null
+    var princeId: String? = null
+    var name: String? = null
+    var age: Int? = null
+    var status: StatusCode? = null
+    var birthDate: String? = null
 
-    public static PrinceDetailDto fromEntity(Prince prince) {
-        return PrinceDetailDto.builder()
-                .princeLevel(prince.getPrinceLevel())
-                .skillType(prince.getSkillType())
-                .experienceYears(prince.getExperienceYears())
-                .princeId(prince.getPrinceId())
-                .name(prince.getName())
-                .age(prince.getAge())
-                .status(prince.getStatus())
-                .birthDate(getLocalDateTimeString(prince.getCreatedAt()))
-                .build();
+
+
+
+
+
+    companion object {
+        @JvmStatic
+        fun fromEntity(prince: Prince): PrinceDetailDto {
+            return PrinceDetailDto.builder()
+                .princeLevel(prince.princeLevel)
+                .skillType(prince.skillType)
+                .experienceYears(prince.experienceYears)
+                .princeId(prince.princeId)
+                .name(prince.name)
+                .age(prince.age)
+                .status(prince.status)
+                .birthDate(getLocalDateTimeString(prince.createdAt!!))
+                .build()
+        }
     }
 }
